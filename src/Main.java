e043c23f95e698033866ee80e51df76f90c62940
Guilherme Faraco <100322219+guifaraco@users.noreply.github.com>
2023-09-16 11:13:38 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe o cep que deseja buscar: ");
+        String cep = sc.nextLine();
+
+        ConsultaCep consultaCep = new ConsultaCep();
+        try {
+            Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+            GeradorDeArquivo gerador = new GeradorDeArquivo();
+            gerador.salvaJson(novoEndereco);
+        } catch (RuntimeException | IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicação.");
+        }
+
+
+    }
+}
